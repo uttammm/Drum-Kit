d495a19a88a49023d6dfbf065e65080ccb50aaba
Uttam M <53312923+uttammm@users.noreply.github.com>
2022-03-24 22:53:02 +0530

    Added Drum kit files

---
+++ index.js
@@ -0,0 +1,78 @@
+var numberOfDrums = document.querySelectorAll(".drum").length;
+
+for(var i = 0; i<numberOfDrums; i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click",function (){
+    switch (this.innerHTML) {
+      case "w":
+        var crash = new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
+      case "a":
+        var kick = new Audio("sounds/kick.mp3");
+        kick.play();
+        break;
+      case "s":
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+      case "d":
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
+      case "j":
+        var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        break;
+      case "k":
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+      case "l":
+        var snare = new Audio("sounds/snare.mp3");
+        snare.play();
+        break;  
+    
+      default: alert(this.innerHTML);
+        break;
+    }
+    
+  });
+}
+
+document.addEventListener("keydown",function(event){
+  switch (event.key) {
+    case "w":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "a":
+      var kick = new Audio("sounds/kick.mp3");
+      kick.play();
+      break;
+    case "s":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "d":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "j":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "k":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "l":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;  
+  
+    default: alert(event);
+      break;
+  }
+})
+
+
