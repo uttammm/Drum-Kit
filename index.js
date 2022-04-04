22e1d42e77c81af571f524c3483239809c73a200
Uttam M <53312923+uttammm@users.noreply.github.com>
2022-04-04 16:23:11 +0530

    Added button press animations

--- index.js
+++ index.js
@@ -2,45 +2,18 @@ var numberOfDrums = document.querySelectorAll(".drum").length;
 
 for(var i = 0; i<numberOfDrums; i++){
   document.querySelectorAll(".drum")[i].addEventListener("click",function (){
-    switch (this.innerHTML) {
-      case "w":
-        var crash = new Audio("sounds/crash.mp3");
-        crash.play();
-        break;
-      case "a":
-        var kick = new Audio("sounds/kick.mp3");
-        kick.play();
-        break;
-      case "s":
-        var tom1 = new Audio("sounds/tom-1.mp3");
-        tom1.play();
-        break;
-      case "d":
-        var tom3 = new Audio("sounds/tom-3.mp3");
-        tom3.play();
-        break;
-      case "j":
-        var tom4 = new Audio("sounds/tom-4.mp3");
-        tom4.play();
-        break;
-      case "k":
-        var tom2 = new Audio("sounds/tom-2.mp3");
-        tom2.play();
-        break;
-      case "l":
-        var snare = new Audio("sounds/snare.mp3");
-        snare.play();
-        break;  
-    
-      default: alert(this.innerHTML);
-        break;
-    }
-    
+    makeSound(this.innerHTML);
+    buttonAnimation(this.innerHTML);
   });
 }
 
 document.addEventListener("keydown",function(event){
-  switch (event.key) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+})
+
+function makeSound(key){
+  switch (key) {
     case "w":
       var crash = new Audio("sounds/crash.mp3");
       crash.play();
@@ -70,9 +43,16 @@ document.addEventListener("keydown",function(event){
       snare.play();
       break;  
   
-    default: alert(event);
+    default: 
       break;
   }
-})
+}
 
+function buttonAnimation(key){
+  var activeButton = document.querySelector("."+key)
+  activeButton.classList.add("pressed");
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  },100)
+}
 
